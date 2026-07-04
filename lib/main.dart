ab30a4e52caf72775cdb9d9f35ed8cecbf4c975e
Sorawit <thankzaki@hotmail.com>
2026-07-04 11:22:51 +0700

    Step 5: Connect to Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 // Step 2: Install Loading app screen
 import 'package:learn_flutter_68_2_2/first_screen.dart';
 
-void main() {
+// Step 5: Connect to Firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+// Step 5: Connect to Firebase
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MaterialApp(home: FirstScreen()));
 }
